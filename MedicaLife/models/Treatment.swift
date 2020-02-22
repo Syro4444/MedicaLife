9a03e3dd7bfcf628cb6723d4bc257e2c7ae6caa9
Norm <norman.96.n@gmail.com>
2020-02-22 18:10:03 +0100

    ajout de date et heure pour le traitement

--- MedicaLife/models/Treatment.swift
+++ MedicaLife/models/Treatment.swift
@@ -11,16 +11,20 @@ import UIKit
 class Treatment: CustomStringConvertible {
     var id: Int?
     var name: String
+    var date: String
+    var hour: String
     var comment: String
     var doseByDay: Int
     
-    init(name: String, comment: String, doseByDay: Int) {
+    init(name: String, date: String, hour: String, comment: String, doseByDay: Int) {
         self.name = name // nom du medicament
+        self.hour = hour // heure de la prise
+        self.date = date // date du début de traitement
         self.comment = comment // info sur me medicament, memo
         self.doseByDay = doseByDay // nombre de pillule par jour par exemple
     }
     
     var description: String {
-        return "{\(self.id ?? 0) \(self.name) \(self.comment) \(self.doseByDay)}"
+        return "{\(self.id ?? 0) \(self.name) \(self.date) \(self.hour) \(self.comment) \(self.doseByDay)}"
     }
 }
--- MedicaLife/sevice/TreatmentFactory.swift
+++ MedicaLife/sevice/TreatmentFactory.swift
@@ -12,11 +12,13 @@ class TreatmentFactory {
     
     static func treatmentFrom(dictionary: [String: Any]) -> Treatment? {
         guard let n = dictionary["name"] as? String,
+              let a = dictionary["date"] as? String,
+              let h = dictionary["hour"] as? String,
               let c = dictionary["comment"] as? String,
               let d = dictionary["doseByDay"] as? Int else {
                 return nil
         }
-        let treatment = Treatment(name: n, comment: c, doseByDay: d)
+        let treatment = Treatment(name: n, date: a, hour: h, comment: c, doseByDay: d)
         treatment.id = dictionary["id"] as? Int
         return treatment
     }
@@ -24,6 +26,8 @@ class TreatmentFactory {
     static func dictionaryFrom(treatment: Treatment) -> [String: Any] {
         return [
             "name": treatment.name,
+            "date": treatment.date,
+            "hour": treatment.hour,
             "comment": treatment.comment,
             "doseByDay": treatment.doseByDay
         ]
