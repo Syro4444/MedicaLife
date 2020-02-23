d6c15d8780adc80ad8e5932db8ec34ad85df0e0e
Norm <norman.96.n@gmail.com>
2020-02-23 19:13:39 +0100

    cell edit

---
+++ MedicaLife/scenes/TreatmentEditViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CellEditViewController.swift
+//  MedicaLife
+//
+//  Created by Norman on 23/02/2020.
+//  Copyright © 2020 raphael. All rights reserved.
+//
+
+import UIKit
+
+class CellEditViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
